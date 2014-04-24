49cadd49b4e5e2a87b50b3cd8512f8f9908245ef
Sam Brotherton <sbrother@gmail.com>
2014-04-24 16:50:05 -0700

    fixes for postgres and ubuntu 12.04

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE articles (
        id SERIAL PRIMARY KEY,
        url TEXT,
-       `section` TEXT,
+       section TEXT,
        byline TEXT,
        title TEXT,
        abstract TEXT,
