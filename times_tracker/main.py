0da037b463aff2a059d291ace82577b72e9592d2
Sam Brotherton <sbrother@gmail.com>
2014-04-24 18:53:08 -0700

    super basic 'search engine'

--- times_tracker/main.py
+++ times_tracker/main.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, request, abort
 app = Flask(__name__)
+import json
+from search import search
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Welcome to the Data Science Web Service!"
+
+@app.route("/search")
+def search_articles():
+    query = request.args.get('q')
+    if query is None:
+        abort(400) # bad request
+    else:
+        return json.dumps(list(search(query)))
 
 if __name__ == "__main__":
-    app.run(host ='0.0.0.0', debug = False)
+    app.run(host ='0.0.0.0', debug = True)
---
+++ times_tracker/search.py
@@ -0,0 +1,19 @@
+import psycopg2
+
+DB_NAME = 'times_tracker'
+DB_USER = 'sam'
+
+conn = psycopg2.connect("dbname=%s user=%s" % (DB_NAME, DB_USER))
+
+def search(term):
+    cur = conn.cursor()
+    # This is terrible, never do this in real code. For several reasons:
+    #     1. Remember when we talked about SQL injection? How could this function be used to 
+    #        compromise our database?
+    #     2. Using LIKE queries is really slow. If you want to implement search, use something 
+    #        like ElasticSearch, which is designed for this sort of thing.
+    cur.execute('''SELECT url, section, byline, title, abstract FROM articles WHERE abstract LIKE '%%%s%%';'''% (term,))
+    for result in cur:
+        print result
+        yield result
+                
